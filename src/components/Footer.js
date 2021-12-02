84b3f0c64d28ac107baa8163abfc2d0df5345601
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-01 23:42:27 -0600

    Add Footer component and Import Footer in App

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div>
+      Built by{' '}
+      <a href="https://woosikchoi.com" target="_blank">
+        Woo-Sik Choi
+      </a>{' '}
+      in December 2021
+    </div>
+  );
+}
+
+export default Footer;
