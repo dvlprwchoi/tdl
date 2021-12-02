a9d13b3b10e4988d4cead3466c03cfdfb3fb3dea
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-02 15:24:24 -0600

    Add addTodo handler function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import About from './components/About/About';
 import TodoList from './components/TodoList/TodoList';
 import Footer from './components/Footer/Footer';
@@ -6,10 +6,24 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([
-    { id: 1, name: 'Todo 1', complete: false },
-    { id: 2, name: 'Todo 2', complete: false },
-    { id: 3, name: 'Todo 3', complete: true },
+    // { id: 1, name: 'Todo 1', complete: false },
+    // { id: 2, name: 'Todo 2', complete: false },
+    // { id: 3, name: 'Todo 3', complete: true },
   ]);
+  const newTodoInputValue = useRef();
+
+  // Handler functions
+  function _addTodo(e) {
+    const newValue = newTodoInputValue.current.value;
+    // console.log(newValue);
+    // Not to add empty list
+    if (newValue === '') return;
+    setTodos((previousTodos) => {
+      return [...previousTodos, { id: 1, name: newValue, complete: false }];
+    });
+    // Clear input after adding
+    newTodoInputValue.current.value = null;
+  }
   return (
     <>
       <div className="About">
@@ -17,9 +31,9 @@ function App() {
       </div>
       <div className="TodoList">
         <TodoList todos={todos} />
-        <input className="input" type="text"></input>
+        <input className="input" type="text" ref={newTodoInputValue}></input>
         <div className="buttons">
-          <button>Add Todo</button>
+          <button onClick={_addTodo}>Add Todo</button>
           <button>Delete Todo</button>
           <button>Clear Completed Todo</button>
         </div>
