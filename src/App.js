141d5ffccef26667174b5b726617e349157e4c48
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-02 14:05:38 -0600

    Update todos array with object

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import { useState } from 'react';
 import About from './components/About/About';
 import TodoList from './components/TodoList/TodoList';
 import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, name: 'Todo 1', complete: false },
+    { id: 2, name: 'Todo 2', complete: false },
+    { id: 3, name: 'Todo 3', complete: true },
+  ]);
   return (
     <>
       <div className="About">
         <About />
       </div>
       <div className="TodoList">
-        <TodoList />
+        <TodoList todos={todos} />
         <input className="input" type="text"></input>
         <div className="buttons">
           <button>Add Todo</button>
@@ -18,7 +24,7 @@ function App() {
           <button>Clear Completed Todo</button>
         </div>
         <div className="status">
-          <div>Total Todo number: 0</div>
+          <div>Total Todo number: {todos.length}</div>
           <div>0 Todo left</div>
         </div>
       </div>
