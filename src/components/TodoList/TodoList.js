8b3bff605d18f419f201476d3cf7982e4d484f14
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-02 13:20:20 -0600

    Add and Import Todo component

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,7 +1,12 @@
-function TodoList() {
+import Todo from '../Todo/Todo';
+
+function TodoList({ todos }) {
   return (
     <div>
       <h1>Todo List</h1>
+      {todos.map((todo) => {
+        return <Todo todo={todo} />;
+      })}
     </div>
   );
 }
