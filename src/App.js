9e2c5823f4bca0e34b27ba627ea8cb70d247eba3
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-02 11:24:23 -0600

    Delete unneccessary codes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import About from './components/About/About';
 import TodoList from './components/TodoList/TodoList';
 import Footer from './components/Footer/Footer';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -12,20 +11,6 @@ function App() {
       </div>
       <div className="TodoList">
         <TodoList />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
       <div className="Footer">
         <Footer />
