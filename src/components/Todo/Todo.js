5a0cc1faf7d8b23178271082dbf5025c82f74085
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-03 09:59:19 -0600

    Add checkbox click handle function

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,5 +1,22 @@
-function Todo({ todo }) {
-  return <div>{todo}</div>;
+function Todo({ todo, toggleCheckbox }) {
+  // Handle function
+  // Checkbox click
+  function _checkboxClick() {
+    // console.log('Checkbox is clicked');
+    toggleCheckbox(todo.id);
+  }
+  return (
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={_checkboxClick}
+        />
+        {todo.name}
+      </label>
+    </div>
+  );
 }
 
 export default Todo;
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,11 @@
 import Todo from '../Todo/Todo';
 
-function TodoList({ todos }) {
+function TodoList({ todos, toggleCheckbox }) {
   return (
     <div>
       <h1>Todo List</h1>
       {todos.map((todo) => {
-        return <Todo todo={todo} />;
+        return <Todo toggleCheckbox={toggleCheckbox} todo={todo} />;
       })}
     </div>
   );
